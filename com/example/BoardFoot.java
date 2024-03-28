e79f033d70628d86b64a131113bb35aab624bd74
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2024-03-28 10:09:59 -0400

    Initial commit

---
+++ com/example/BoardFoot.java
@@ -0,0 +1,26 @@
+
+/**
+ * Change me.
+ *
+ * @author Kevin Csiffary
+ * @version 1.0
+ * @since 2024-03-28
+ */
+
+// BoardFoot class
+public final class BoardFoot {
+
+    /** Private constructor to prevent instantiation. */
+    private BoardFoot() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
